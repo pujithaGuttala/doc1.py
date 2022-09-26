4a89f7d6394bc84a20736ea7cfb5f0ca94a31d8d
pujithaGuttala <108428114+pujithaGuttala@users.noreply.github.com>
2022-09-26 15:03:30 +0530

    Add files via upload

---
+++ doc2.py
@@ -0,0 +1,15 @@
+# a="w3resource"
+# b={}
+# for i in a:
+#     if i in b:
+#         b[i]+=1
+#     else:
+#         b[i]=1
+# print(b)
+# {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
+
+
+
+a={"fruits":"vegetable","piuij":"reddy","cvbn":"asdf"}
+for i in a:
+    print(a[i])
